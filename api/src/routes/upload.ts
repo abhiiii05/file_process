f38ecbdb13cc952905ef9cff274bb34a728697a8
abhiiii.iiii <abhijithviju2005@gmail.com>
2026-03-14 20:22:38 +0530

    upload route

---
+++ api/src/routes/upload.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+import type { Request, Response } from "express";
+import multer from 'multer';
+import crypto from 'crypto';
+import fs from 'fs';
+import { db } from '../db';
+import { file, jobs } from '../db/schema';
+
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: (_, file, cb) => { 
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+})
+
+const upload = multer({storage})
+
+router.post('/upload', upload.single('file'),  async (req: Request, res: Response) => {
+  try {
+    const uploadFile = req.file;
+    if (!uploadFile) {
+      return res.status(400).send('No file uploaded.');
+    }
+    const fileBuffer = fs.readFileSync(uploadFile.path);
+    const hash = crypto.createHash('sha256').update(fileBuffer).digest('hex');// checksum
+    const newFile = await db.insert(file).values({
+      userId: "7fa07482-cf96-44eb-a749-0b0b99238a93",
+      fileName: uploadFile.originalname,
+      fileSize: uploadFile.size,
+      storagePath: uploadFile.path,
+      checksum: hash,
+    }).returning();
+    
+    const insertedFile = newFile[0];
+    
+    if (!insertedFile) {
+      throw new Error("File insert failed");
+    }
+    
+    await db.insert(jobs).values({
+      fileId: insertedFile.id,
+    });
+    
+    res.json({
+          message: "File uploaded",
+          fileId: insertedFile.id
+        })
+    
+  }
+  
+  catch (error) {
+    console.error`(error)
+    return res.status(500).send('Internal Server Error');
+  }
+})
+
+export default router;
